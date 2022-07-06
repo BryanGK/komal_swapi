7d169bb4f20ed9b7d7916cc9815dad3326930a4d
Komal Gill <komalgill0310@gmail.com>
2022-07-06 14:07:22 -0400

    able to use map function on the returned response from axios.get request

--- src/App.js
+++ src/App.js
@@ -21,20 +21,24 @@ export default function App() {
   async function swapiData() {
     const swapiApi = await axios.get("https://swapi.dev/api/"); //return an object, which is why there is no need to convert the given data into JSON object
     // const response = await swapiApi.json();
+
     const response = await swapiApi.data.people; //output: https://swapi.dev/api/people/
     // console.log(response);
+
     const fetchData = await axios.get(response); //fetching data in form of an object from people's Url
     // console.log("fetchData: ", fetchData)
+
     const characterName = await fetchData.data.results; //pulling up the array of object data of people
-    // console.table("Name: ", characterName);
+    // console.table("Name: ", typeof characterName);
     return characterName;
   }
 
-  function assignTableData() {
-    let swapiMap = swapiData(); //type is object
-    let arrData = Object.entries(swapiMap); //returning an empty array => Why it returns an empty array
-    let eachTableData = arrData.map((eachPerson) => eachPerson.name); //map only works on Arrays
-    console.log(eachTableData); //Why it's an empty array?
+ async function assignTableData() {
+    let swapiMap = await swapiData(); //type is object
+    // console.log(swapiMap)
+    // let arrData = Object.entries(swapiMap); //returning an empty array => Why it returns an empty array
+    let eachTableData = swapiMap.map((eachPerson) => eachPerson.name); //map only works on Arrays
+    console.log(eachTableData); 
   }
   useEffect(() => {
     assignTableData();
